14097fd78135466718ee625cbcd43c3689742c85
Laurent Georget <laurent.georget@inria.fr>
2015-06-17 18:59:09 +0200

    Reindent properly viewer.h

--- KayrebtViewer/viewer.h
+++ KayrebtViewer/viewer.h
@@ -10,46 +10,46 @@
 #include "graphitemmodel.h"
 
 namespace Ui {
-  class Viewer;
+	class Viewer;
 }
 
 class GraphItem;
 
 class Viewer : public QMainWindow
 {
-  Q_OBJECT
+	Q_OBJECT
 
 public:
-  explicit Viewer(QWidget *parent = 0);
-  ~Viewer();
+	explicit Viewer(QWidget *parent = 0);
+	~Viewer();
 
-public slots:
-  void openGraph();
-  void openGraph(const QString &filename);
+	public slots:
+	void openGraph();
+	void openGraph(const QString &filename);
 
 signals:
-  void newGraphOpen(const GraphItem& file);
+	void newGraphOpen(const GraphItem& file);
 
 private:
-  Ui::Viewer *ui;
-  QDockWidget* _configDock;
-  QDockWidget* _sourcesDock;
-  QDockWidget* _databaseDock;
-  SourceTreeWidget* _srcTreeWidget;
-  DatabaseViewer* _dbviewer;
-//  QList<Drawing *> _scenes;
+	Ui::Viewer *ui;
+	QDockWidget* _configDock;
+	QDockWidget* _sourcesDock;
+	QDockWidget* _databaseDock;
+	SourceTreeWidget* _srcTreeWidget;
+	DatabaseViewer* _dbviewer;
+	//  QList<Drawing *> _scenes;
 
-  QString _srcTree;
-  QString _symbDb;
-  QString _diagdir;
-  GraphItemModel _openGraphs;
+	QString _srcTree;
+	QString _symbDb;
+	QString _diagdir;
+	GraphItemModel _openGraphs;
 
-  static quint64 _graphsIdGenerator;
+	static quint64 _graphsIdGenerator;
 
-  quint64 doOpenGraph(const QFileInfo &file);
+	quint64 doOpenGraph(const QFileInfo &file);
 
- protected:
-  virtual bool event(QEvent *event);
+protected:
+	virtual bool event(QEvent *event);
 };
 
 #endif // VIEWER_H
